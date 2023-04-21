da1f43be45d004e00a6f3f458658be63ff6ed41c
Calvin <theis530@umn.edu>
2023-04-21 13:04:48 -0500

    Implement Reservice

--- libs/transit/include/Reservice.h
+++ libs/transit/include/Reservice.h
@@ -1,1 +1,23 @@
-//   std::vector<RechargerDrone*> rechargerDroneLists;
+#ifndef RESERVICE_H_
+#define RESERVICE_H_
+
+#include "RechargerDrone.h"
+#include "IEntity.h"
+#include <vector>
+
+class Reservice {
+public:
+  Reservice();
+  Reservice* GetInstance();
+
+  RechargerDrone* FindNearestAvailableRechargerDrone(IEntity*);
+
+  void AddRechargerDrone(RechargerDrone* drone);
+  void RemoveRechargerDrone(RechargerDrone* drone);
+
+private:
+  std::vector<RechargerDrone *> rechargerDrones;
+  static Reservice *instance;
+};
+
+#endif
---
+++ libs/transit/src/Reservice.cc
@@ -0,0 +1,42 @@
+#include "Reservice.h"
+#include "RechargerDrone.h"
+#include <algorithm>
+
+Reservice::Reservice() {
+  Reservice::instance = this;
+}
+
+Reservice* Reservice::GetInstance() {
+  return Reservice::instance;
+}
+
+void Reservice::AddRechargerDrone(RechargerDrone *drone_to_add) {
+  this->rechargerDrones.push_back(drone_to_add);
+}
+
+void Reservice::RemoveRechargerDrone(RechargerDrone *drone_to_delete) {
+  auto iter = std::find(
+      this->rechargerDrones.begin(),
+      this->rechargerDrones.end(),
+      drone_to_delete);
+  this->rechargerDrones.erase(iter);
+}
+
+RechargerDrone* Reservice::FindNearestAvailableRechargerDrone(IEntity *drone) {
+  int min_distance = 0;
+
+  RechargerDrone *nearest_recharger_drone = nullptr;
+  
+  int distance = 0;
+  for (auto recharger_drone : this->rechargerDrones) {
+    if (recharger_drone->GetAvailability()) {
+      distance = recharger_drone->GetPosition().Distance(drone->GetPosition()); 
+      if (distance <= min_distance) {
+        min_distance = distance;
+        nearest_recharger_drone = recharger_drone;
+      }
+    }
+  }
+
+  return nearest_recharger_drone;
+}
