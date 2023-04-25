9afc3f7e36615aad5bb1ed936ae648418ac8f7f7
Calvin <theis530@umn.edu>
2023-04-25 11:49:44 -0500

    Format DataCollection.h

--- libs/transit/include/DataCollection.h
+++ libs/transit/include/DataCollection.h
@@ -12,33 +12,34 @@
  * simulation.
  */
 class DataCollection { 
-  private: DataCollection() {};
+private: 
+  DataCollection() {};
+    
+  static DataCollection* instance;  // The single instance of DataCollection
+  std::list<struct TripData> trips; // The trip data
+  int totalDistance;                // Total distance traveled
+  int totalRecharges;               // The total number of drone recharge sessions
 
-     static DataCollection* instance;  // The single instance of DataCollection
-     std::list<struct TripData> trips; // The trip data
-     int totalDistance;                // Total distance traveled
-     int totalRecharges;               // The total number of drone recharge sessions
+public:
+  // Delete copy constructor & assignment operator.
+  DataCollection(const DataCollection& o) = delete;
+  DataCollection* operator=(const DataCollection& o) = delete;
 
-  public:
-     // Delete copy constructor & assignment operator.
-     DataCollection(const DataCollection& o) = delete;
-     DataCollection* operator=(const DataCollection& o) = delete;
-
-     /**
+  /**
       * Gets the single instance of DataCollection.
       */
-     static DataCollection* getInstance();
+  static DataCollection* getInstance();
 
-     /**
+  /**
       * Adds a trip the list of trips.
       * @param trip The trip to add.
       */
-     void addTrip(struct TripData* trip);
+  void addTrip(struct TripData* trip);
 
-     /**
+  /**
       * @brief Uses data collected to create CSV file.
       */
-     void outputCSVFile();
+  void outputCSVFile();
 };
 
 #endif
